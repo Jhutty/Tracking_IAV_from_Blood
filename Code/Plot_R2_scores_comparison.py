488bc0e983578e56858071a24027279ca61f3fa9
Suneet Singh Jhutty <jhutty@fias.uni-frankfurt.de>
2022-02-21 19:26:10 +0100

    change font size

--- Code/Plot_R2_scores_comparison.py
+++ Code/Plot_R2_scores_comparison.py
@@ -35,13 +35,14 @@ df_plot.sort_values(by="r2 model", inplace=True)
 # create plot
 fig, ax = plt.subplots()
 fig.set_size_inches(14, 10)
-plt.grid()
-siz=20
+#plt.grid()
+siz=25
 plt.ylabel(r"$R^2$ score", size=siz+2)
 plt.axhline(y=0, color="grey", linestyle='--')
 plt.plot(df_plot["r2 mean"], 'kd', label="benchmark", markersize=11, color = "k")
 plt.plot(df_plot["r2 model"], 'bd', label="model", markersize=11, color="#5974B3")
 plt.xticks(np.arange(len(name)), size=13)
+plt.yticks(size=16)
 ax.set_xticklabels(df_plot.index, fontsize=siz-6)
 plt.legend(loc="lower right", fontsize=siz-4, frameon=True)
 plt.savefig("../Plots/R2_scores_comparison/R2_scores_comparison.png", dpi=300, bbox_inches="tight")
